f464cd19b24bac6ec8c31d457585ea29df14ce68
Ivan <carivan14@homtail.com>
2024-02-18 21:57:24 -0300

    Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center min-[300]:w-[20rem] ">
+    <main className="flex min-h-screen flex-col items-center justify-center ">
       <LoginForm />
     </main>
   );
